068cbf2654fe6064c16b8cd2d714ef3bb76867fd
NaveenAkash-K <64596821+NaveenAkash-K@users.noreply.github.com>
2023-04-05 21:16:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function openmenu(){
+   var x = document.getElementById('nav-mob-list');
+
+   if(x.style.display=='none')
+      x.style.display='block';
+   else{
+      x.style.display='none';
+   }
+      // document.getElementById('nav-mob-list').style.display='none';
+}
