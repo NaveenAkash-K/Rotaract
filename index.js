d6a6af8dc38a34edca7f6b05cce614eea5e7e22e
NaveenAkash-K <64596821+NaveenAkash-K@users.noreply.github.com>
2023-04-07 22:49:00 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+var x = document.getElementById('nav-mob-list');
+
 function openmenu(){
-   var x = document.getElementById('nav-mob-list');
 
    if(x.style.display=='none')
       x.style.display='block';
@@ -7,4 +8,13 @@ function openmenu(){
       x.style.display='none';
    }
       // document.getElementById('nav-mob-list').style.display='none';
+}
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < currentScrollPos) {
+   x.style.display='none';
+    
+  }
 }
